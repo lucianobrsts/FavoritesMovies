1e7b05f106120fddc975db51af13d99f2ab2e6f4
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-06-06 18:10:16 -0300

    Acrescentado novas funcionalidades video, favoritos.

---
+++ app/src/main/java/unit7/dev/favoritesmovies/model/TrailerResponse.java
@@ -0,0 +1,25 @@
+package unit7.dev.favoritesmovies.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TrailerResponse {
+
+    @SerializedName("id")
+    private int id_trailer;
+    @SerializedName("results")
+    private List<Trailer> results;
+
+    public int getIdTrailer(){
+        return id_trailer;
+    }
+
+    public void seIdTrailer(int id_trailer){
+        this.id_trailer = id_trailer;
+    }
+
+    public List<Trailer> getResults(){
+        return results;
+    }
+}
